b0bbd22bc7be7580def5f2506e21b91300d91a1c
Etnavas <km6104@nyu.edu>
2025-10-20 14:58:07 -0400

    Update 1
    Set up + initial ideas

---
+++ sketch.js
@@ -0,0 +1,11 @@
+//word is Mellow
+//colorfull night lights that change slowly transitioning into different colors 
+//peacefull music maybe?? piano, 
+
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(220);
+}
