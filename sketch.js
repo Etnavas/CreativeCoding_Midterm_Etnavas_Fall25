a988d518e2f9f98294f1c93a741cbe7b56ca56a9
Etnavas <km6104@nyu.edu>
2025-10-20 15:05:07 -0400

    Update 2
    Added project must-haves to keep in mind

--- sketch.js
+++ sketch.js
@@ -1,11 +1,24 @@
-//word is Mellow
-//colorfull night lights that change slowly transitioning into different colors 
-//peacefull music maybe?? piano, 
+/*
+Word is Mellow
+colorfull night lights that change slowly transitioning into different colors 
+peacefull music maybe?? piano, melodic perciusion perhaps (i can't spell lol)
+
+Must include: 
+• heavy use of variables custom functions passing variables as parameter
+• loops
+• control structures
+• math or random
+• some use of interaction or change over time
+• movement, change, variety
+• arrays
+• conditionals
+• OOP - with methods + passing parameters
+*/
 
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(800, 800);
 }
 
 function draw() {
-  background(220);
+  background(0);
 }
