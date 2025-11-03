b435f66877e86e19e6a95a9c3465b19813eb2e3f
Etnavas <km6104@nyu.edu>
2025-11-02 23:23:15 -0500

    Update 7
    Still figuring things out

--- Class.js
+++ Class.js
@@ -8,19 +8,26 @@ class Particles {
         this.x = random(width); //x value is a random canvas width number 
         this.y = random(height); //y value is a random canvas height number 
         this.size = random(30, 120); //size is randomly between 30 120
+        //this.offset(1000);
         //this.speed = random(range in millisecons?) 
-        //something to offset the start time of the circles here --->
+        //something to offset the start time of the circles here --->   
+        //this.offset(1000);
+
         this.color = color(random(100,255), random(100,255), random(200,255)); // didn't want colors to be too dark
         
     }
 
     
     display () {
-        //add variables 
+        let t = millis();
+        //let alpha = ...
+
+         //add variables 
         fill(red(this.color), green(this.color), blue(this.color), alpha);
         ellipse(this.x, this.y, this.size);
     }
-
+    
+    //maybe I should use an update function?
     //fadding overtime?
     //how can I incorporate millis? 
 
