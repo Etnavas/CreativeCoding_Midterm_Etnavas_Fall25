3de7c1d5d312ffaede6d63be3cc90513a3bb1633
Etnavas <km6104@nyu.edu>
2025-11-06 10:30:46 -0500

    Update 14
    Added pop function (Thanks Bo! :D )
    
    Goals:
    - Fix fade in logic
    - bubble movement if time allows

--- Class.js
+++ Class.js
@@ -1,76 +1,156 @@
 
 
+// class Bubbles {
+
+//     constructor(x,y,s) {
+//         this.x = x;//x value is a random canvas width number 
+//         this.y = y; //y value is a random canvas height number 
+//         this.size = s;//size is randomly between 30 120
+//         this.color = color(random(100,255), random(100,255), random(200,255)); // blue value (higher range = more blue/purple) 
+//         this.startTime = millis() + random (5000); // random delay before each circle starts fading
+//         this.speed = 3500; //how fast it fades and out
+//         this.alpha = 0; // Transparency (start invicible)
+//     }
+    
+
+//      Update function
+//     update () {
+      
+//         // time passed / how long is it been active? 
+//       let tPassed = millis() - this.startTime;
+
+//       //cycle - fade in + fade out
+//       let cycle = this.speed * 3; 
+
+//       let r = tPassed % cycle; // "r" will always reset to 0 after a full cycle
+
+//       //repetition 
+
+      
+//       //fade in is only works when the sketch is first running FIX THAT 
+
+//       //fade in
+//       if ( tPassed < this.speed) {
+//         this.alpha = map (r, 0, this.speed, 0, 255 ) // the value of r is mapped into a range between 0 & 255
+
+
+//       } 
+//       //fade out
+//       else {
+//         this.alpha = map (r, this.speed, cycle, 255, 0 ); // the value of r is mapped into a range between 255 & 0
+
+//       }
+
+//     }
+
+//     //Maybe bubbles could slowly move around the screen before fading 
+//     // I would need each bubble to start at a random point and slowly change each of the x and y values of each object in my array
+//     // I could use random() and give each one a small range for their movement but I would need to find a way for each to have their own range based of their initial possition 
+
+
+//     display () {
+        
+//         fill(red(this.color), green(this.color), blue(this.color), this.alpha);
+//         ellipse(this.x, this.y, this.size);
+//     }
+//     conatains(px,py){
+//       let d = dist(px,py,this.x,this.y);
+//       return d < this.size;
+//     }
+    
+//    /*
+
+//     //maybe this can be under update? 
+//     function pop () {
+
+//       // if circles are pressed then they will pop like bubbles 
+//       let value = 0 
+  
+//       function doubleClicked() {
+//         // how can I specify which object and apply that to each object in my array? 
+  
+//       }
+    
+//     }
+   
+//    */
+
+
+   
+// }
+
 class Bubbles {
 
-    constructor(parameters) {
-        this.x = random(width); //x value is a random canvas width number 
-        this.y = random(height); //y value is a random canvas height number 
-        this.size = random(30, 120); //size is randomly between 30 120
-        this.color = color(random(100,255), random(100,255), random(200,255)); // blue value (higher range = more blue/purple) 
-        this.startTime = millis() + random (5000); // random delay before each circle starts fading
-        this.speed = 3500; //how fast it fades and out
-        this.alpha = 0; // Transparency (start invicible)
-    }
-    
-
-     //Update function
-    update () {
-      
-        // time passed / how long is it been active? 
-      let tPassed = millis() - this.startTime;
-
-      //cycle - fade in + fade out
-      let cycle = this.speed * 3; 
-
-      let r = tPassed % cycle; // "r" will always reset to 0 after a full cycle
-
-      //repetition 
-
-      
-      //fade in is only works when the sketch is first running FIX THAT 
-
-      //fade in
-      if ( tPassed < this.speed) {
-        this.alpha = map (r, 0, this.speed, 0, 255 ) // the value of r is mapped into a range between 0 & 255
-
-
-      } 
-      //fade out
-      else {
-        this.alpha = map (r, this.speed, cycle, 255, 0 ); // the value of r is mapped into a range between 255 & 0
-
-      }
-
-    }
-
-    //Maybe bubbles could slowly move around the screen before fading 
-    // I would need each bubble to start at a random point and slowly change each of the x and y values of each object in my array
-    // I could use random() and give each one a small range for their movement but I would need to find a way for each to have their own range based of their initial possition 
-    
-
-    display () {
-        
-        fill(red(this.color), green(this.color), blue(this.color), this.alpha);
-        ellipse(this.x, this.y, this.size);
-    }
-    
-   /*
-
-    //maybe this can be under update? 
-    function pop () {
-
-      // if circles are pressed then they will pop like bubbles 
-      let value = 0 
+  constructor(x, y, s) {
+    this.x = x;
+    this.y = y;
+    this.size = s;
+    this.color = color(random(100, 255), random(100, 255), random(200, 255));
+    this.startTime = millis() + random(5000);
+    this.speed = 3500;
+    this.alpha = 0;
+  }
   
-      function doubleClicked() {
-        // how can I specify which object and apply that to each object in my array? 
-  
-      }
+  update () {
+      
+    //         // time passed / how long is it been active? 
+       let tPassed = millis() - this.startTime;
     
+       //cycle - fade in + fade out
+           let cycle = this.speed * 3; 
+
+        let r = tPassed % cycle; // "r" will always reset to 0 after a full cycle
+    
+    //       //repetition 
+    
+          
+    //       //fade in is only works when the sketch is first running FIX THAT 
+    
+    //       //fade in
+           if ( tPassed < this.speed) {
+            this.alpha = map (r, 0, this.speed, 0, 255 ) // the value of r is mapped into a range between 0 & 255
+    
+    
+          } 
+          //fade out
+          else {
+             this.alpha = map (r, this.speed, cycle, 255, 0 ); // the value of r is mapped into a range between 255 & 0
+    
+           }
+    
+         }
+
+
+
+  /*
+  update() {
+    this.x+=1;
+  }
+
+  fade(){
+    let tPassed = millis() - this.startTime;
+    let cycle = this.speed * 2;
+    let r = tPassed % cycle;
+
+    // fade in
+    if (r < this.speed) {
+      this.alpha = map(r, 0, this.speed, 0, 255);
+    } else {
+      // fade out
+      this.alpha = map(r, this.speed, cycle, 255, 0);
     }
-   
-   */
+  }
 
+  */
+  display() {
+    fill(red(this.color), green(this.color), blue(this.color), this.alpha);
+    ellipse(this.x, this.y, this.size);
+  }
 
-   
+  // <-- fixed name + fixed radius math
+  contains(px, py) {
+    let d = dist(px, py, this.x, this.y);
+    // this.size is diameter, so radius is this.size / 2
+    return d < this.size / 2;
+  }
 }
--- sketch.js
+++ sketch.js
@@ -16,37 +16,82 @@ Must include:
 • OOP - with methods + passing parameters
 
 */
+// let circles = [];
+
+// function setup() {
+//   createCanvas(800, 800);
+//   noStroke();
+//   x = width / 2;
+//   y = height / 2;
+
+//   // Create a bunch of circles with random properties
+//   // as long as i is less than 100 create a new Particle and add it to the circles array
+//   for (let i = 0; i < 40; i++) {
+//     // 40 places in my array technically 39
+//     let x = random(width); //x value is a random canvas width number
+//     let y = random(height); //y value is a random canvas height number
+//     let s = random(30, 120); //size is randomly between 30 120
+//     circles.push(new Bubbles(x,y,s)); // Particles() creates new Particle object circles.push pushes it into the end of the array
+//   }
+
+//   // remove objects from my array (idk if this would go on setup it may go on draw loop)
+// }
+
+// function draw() {
+//   //background has less transparency to leave a faither trail
+//   for (let i = 0; i < circles.length; i++) {
+//     //goes through every object in the circles array and displays it
+//     circles[i].update();
+//     circles[i].display();
+//     background(10, 10, 25, 20); //background has less transparency to leave a fainter trail
+//     //circles[i].update();
+//   }
+// }
+
+// function mousePresserd(){
+//   for (let i = circles.length - 1; i>=0; i--){
+//     if (circles[i].contains(mouseX,mouseY)){
+//       circles.splice(i,1);
+//       break; 
+//     }
+//   }
+// }
+
+
+
 let circles = [];
 
 function setup() {
   createCanvas(800, 800);
   noStroke();
-  x = width / 2;
-  y = height / 2;
 
-  // Create a bunch of circles with random properties
-  // as long as i is less than 100 create a new Particle and add it to the circles array
-  for (let i = 0; i < 40; i++) {  // 40 places in my array technically 39 
-    circles.push(new Bubbles()); // Particles() creates new Particle object circles.push pushes it into the end of the array
-    
+  for (let i = 0; i < 40; i++) {
+    let x = random(width);
+    let y = random(height);
+    let s = random(30, 120);
+    circles.push(new Bubbles(x, y, s));
   }
-
- // remove objects from my array (idk if this would go on setup it may go on draw loop)
-
-
 }
 
 function draw() {
+  // put background once per frame
+  background(10, 10, 25, 20);
 
-  //background has less transparency to leave a faither trail 
-
-  for (let i = 0; i < circles.length; i++) { //goes through every object in the circles array and displays it
+  // update + display all bubbles
+  for (let i = 0; i < circles.length; i++) {
     circles[i].update();
+    //circles[i].fade();
     circles[i].display();
-    background(10, 10, 25, 20);//background has less transparency to leave a fainter trail
-    //circles[i].update();
-     
   }
-
 }
 
+// you can also use mousePressed() – you used doubleClicked() so I kept it
+function doubleClicked() {
+  // loop backwards so removing is safe
+  for (let i = circles.length - 1; i >= 0; i--) {
+    if (circles[i].contains(mouseX, mouseY)) {
+      circles.splice(i, 1); // remove this bubble
+      break; // stop after popping one
+    }
+  }
+}
